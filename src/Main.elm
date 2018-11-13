26cbf52999602e8465b75267cdcab671bc3f4d68
samhstn <sam.houston@elixirrcreative.com>
2018-11-13 23:43:26 +0000

    counter example

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,65 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
 
 
 main =
-    text "Hello world"
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+init : Model
+init =
+    0
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = NoOp
+    | Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NoOp ->
+            model
+
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view : Model -> Html Msg
+view model =
+    div
+        []
+        [ div
+            []
+            [ text (String.fromInt model)
+            ]
+        , div
+            []
+            [ button
+                [ onClick Increment
+                ]
+                [ text "+"
+                ]
+            , button
+                [ onClick Decrement
+                ]
+                [ text "-"
+                ]
+            ]
+        ]
