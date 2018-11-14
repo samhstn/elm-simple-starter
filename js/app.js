f25d5a0c11c1bd3a896c4c56cb3ef8f07d045c3c
samhstn <sam.houston@elixirrcreative.com>
2018-11-14 00:02:42 +0000

    counter with localstorage

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 var {Elm} = require('./elm.js');
 
+var flags
+
+try {
+  flags = JSON.parse(localStorage.getItem('cache') || '0')
+} catch (e) {
+  flags = 0
+}
+
 var app_node = document.querySelector('#app');
 var app = Elm.Main.init({
-  node: app_node
+  node: app_node,
+  flags: flags
 });
+
+app.ports.cache.subscribe(function (data) {
+  localStorage.setItem('cache', JSON.stringify(data))
+})
--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,33 @@
-module Main exposing (main)
+port module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Json.Encode as Encode
 
 
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
+        , subscriptions = subscriptions
         , update = update
         , view = view
         }
 
 
-init : Model
-init =
-    0
+port cache : Encode.Value -> Cmd msg
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( flags, Cmd.none )
 
 
 type alias Model =
+    Int
+
+
+type alias Flags =
     Int
 
 
@@ -26,19 +35,23 @@ type Msg
     = NoOp
     | Increment
     | Decrement
+    | Reset
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model
+            ( model, Cmd.none )
 
         Increment ->
-            model + 1
+            ( model + 1, cache (Encode.int (model + 1)) )
 
         Decrement ->
-            model - 1
+            ( model - 1, cache (Encode.int (model - 1)) )
+
+        Reset ->
+            ( 0, cache (Encode.int 0) )
 
 
 view : Model -> Html Msg
@@ -61,5 +74,15 @@ view model =
                 ]
                 [ text "-"
                 ]
+            , button
+                [ onClick Reset
+                ]
+                [ text "reset"
+                ]
             ]
         ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
